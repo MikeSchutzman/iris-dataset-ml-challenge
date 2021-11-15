7d452a3ef4e1b0c94cef5e55b0c6de8b88e3a2d5
Michael Schutzman <michaelschutzman@gmail.com>
2021-11-15 10:25:19 -0500

    elbow method

--- k_means.py
+++ k_means.py
@@ -5,7 +5,6 @@ import logging
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 
@@ -16,11 +15,23 @@ X = pd.read_csv('test_data.csv')
 scalar = StandardScaler()
 X = scalar.fit_transform(X)
 
-# dim reduction to plot in 2D
-pca = PCA(n_components=2)
-X = pca.fit_transform(X)
+# use elbow method to find max k
+error = {}
+for k in range(1, 11):
+    kmeans = KMeans(
+        n_clusters=k,
+        n_init=10,
+        random_state=None,
+    ).fit(X)
+    error[k] = kmeans.inertia_
 
-# get preds
+# plot the results
+plt.plot(list(error.keys()), list(error.values()))
+plt.xlabel('K value')
+plt.ylabel('Error')
+plt.show()
+
+# get preds using optimal k of 3
 kmeans_labels = KMeans(
     n_clusters=3,
     n_init=10,
@@ -28,11 +39,5 @@ kmeans_labels = KMeans(
 ).fit_predict(X)
 logging.info("Predicted labels: %s", kmeans_labels)
 
-# plot the results
-plt.scatter(X[:, 0], X[:, 1], c=kmeans_labels)
-plt.xlabel('pca first feature')
-plt.ylabel('pca second feature')
-plt.show()
-
 # FINAL RESULTS:
-# [0 0 0 0 0 0 0 0 0 0 1 1 2 2 2 2 2 1 2 2 1 2 2 1 2 1 2 2 1 1]
+# [1 1 1 1 1 1 1 1 1 1 2 2 0 0 0 0 0 2 0 0 2 0 0 2 0 2 0 0 2 2]
